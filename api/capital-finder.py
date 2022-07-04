a37721d03ab1a007c8ad4f365fd82b50a9748155
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-07-04 23:16:56 +0300

    testing

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -8,12 +8,15 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
+        print(dic,"dic")
         definitions = []
         if 'word' in dic:
             word = dic['word']
+            print(word, "word")
             url = 'https://restcountries.com/v3.1/capital/'
             r = requests.get(url + word)
             data = r.json()
+            print(data,"wordwordwordwordwordwordwordwordword")
             for word_data in data:
                 definition = word_data['meanings'][0]['definitions'][0]['definition']
                 definitions.append(definition)
